1063a7f424a1b87d80e8f5824aba7716e84c0317
parashuram2504@gmail.com <parshu@DESKTOP-14SED6P.localdomain>
2026-05-24 10:47:30 +0000

    testing webhook

--- app.py
+++ app.py
@@ -3,6 +3,6 @@ from flask import Flask
 app = Flask(__name__)
 @app.route('/')
 def home():
-        return "DevOps Pipeline Working Successfully 🚀"
+        return "DevOps Pipeline Working Successfully"
     if __name__ == "__main__":
             app.run(host='0.0.0.0', port=5000)
